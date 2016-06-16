152926e0c053ce5aa04d283a9492e7c2e320a9a8
Connor Weng <foamtea30@gmail.com>
2016-06-16 21:50:13 +0800

    draft out item page

--- item.js
+++ item.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  Text
+  View,
+  Image,
+  Text,
+  TouchableHighlight
 } from 'react-native';
 
 class Item extends Component {
   render() {
     return (
-      <Text>{this.props.data.goodsName}</Text>
+      <View>
+        <Image style={styles.pic} source={{uri: this.props.data.defaultImage}}/>
+        <Text>{this.props.data.goodsName}</Text>
+        <Text>{this.props.data.price}</Text>
+        <TouchableHighlight onPress={this.upload.bind(this)}>
+          <Text style={styles.uploadFont}>上传</Text>
+        </TouchableHighlight>
+      </View>
     );
   }
+
+  upload() {
+
+  }
 }
+
+const styles = StyleSheet.create({
+  pic: {
+    width: 200,
+    height: 200
+  },
+  uploadFont: {
+    fontSize: 20
+  }
+});
 
 module.exports = Item;
