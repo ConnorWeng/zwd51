9ad68118212f22371a2b673f2a21b36138fa74d4
Connor Weng <foamtea30@gmail.com>
2016-07-07 23:26:29 +0800

    update itempage

--- components/ItemPage.js
+++ components/ItemPage.js
@@ -5,35 +5,138 @@ import {
   View,
   Image,
   Text,
-  TouchableHighlight
+  TouchableHighlight,
+  Dimensions,
+  ScrollView,
+  WebView
 } from 'react-native';
 
+const BODY_TAG_PATTERN = /\<\/ *body\>/;
+
+const script = `
+;(function() {
+var wrapper = document.createElement("div");
+wrapper.id = "height-wrapper";
+while (document.body.firstChild) {
+    wrapper.appendChild(document.body.firstChild);
+}
+document.body.appendChild(wrapper);
+var i = 0;
+function updateHeight() {
+    document.title = wrapper.clientHeight;
+    window.location.hash = ++i;
+}
+updateHeight();
+window.addEventListener("load", function() {
+    updateHeight();
+    setTimeout(updateHeight, 1000);
+});
+window.addEventListener("resize", updateHeight);
+}());
+`;
+
+const style = `
+<style>
+body, html, #height-wrapper {
+    margin: 0;
+    padding: 0;
+}
+#height-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+}
+img {
+    width: 320;
+}
+</style>
+<script>
+${script}
+</script>
+`;
+
+const codeInject = (html) => html.replace(BODY_TAG_PATTERN, style + "</body>");
+
 class ItemPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      webViewHeight: 600
+    };
+  }
+
   render() {
+    let html = '<html><head><title></title></head><body>' + this.props.description + '</body></html>';
     return (
-      <View>
-        <Image style={styles.pic} source={{uri: this.props.defaultImage}}/>
-        <Text>{this.props.goodsName}</Text>
-        <Text>{this.props.price}</Text>
-        <TouchableHighlight onPress={this.upload.bind(this)}>
-          <Text style={styles.uploadFont}>上传</Text>
+      <ScrollView>
+        <Image style={styles.itemImage} source={{uri: this.props.defaultImage}}/>
+        <View style={styles.itemHead}>
+          <Text numberOfLines={2} style={styles.itemTitle}>{this.props.goodsName}</Text>
+          <Text style={styles.itemPrice}>¥ {this.props.price}</Text>
+          <Text style={styles.itemOriginPrice}>淘宝价 ¥ {this.props.price}</Text>
+        </View>
+        <TouchableHighlight style={styles.itemSku}>
+          <Text style={styles.pleaseSelect}>选择  尺码 颜色分类</Text>
         </TouchableHighlight>
-      </View>
+        <WebView style={[styles.itemDesc, {height: this.state.webViewHeight}]}
+                 javaScriptEnabled={true}
+                 onNavigationStateChange={(navState) => {
+                   this.setState({webViewHeight: parseInt(navState.title, 10) || 0});
+                   console.log(navState);
+                 }}
+                 source={{html: codeInject(html)}}/>
+      </ScrollView>
     );
   }
 
-  upload() {
+  taobaoUpload() {
     NativeModules.AlibabaAPI.login();
   }
+
 }
 
+var { height, width } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
-  pic: {
-    width: 200,
-    height: 200
+  itemImage: {
+    width: width,
+    height: 400
   },
-  uploadFont: {
-    fontSize: 20
+  itemHead: {
+    marginTop: 5,
+    paddingLeft: 10,
+    backgroundColor: '#ffffff'
+  },
+  itemTitle: {
+    fontSize: 16,
+    color: '#000000'
+  },
+  itemPrice: {
+    fontSize: 16,
+    color: '#f40'
+  },
+  itemOriginPrice: {
+    fontSize: 14,
+    textDecorationLine: 'line-through'
+  },
+  itemSku: {
+    marginTop: 5,
+    paddingLeft: 10,
+    backgroundColor: '#ffffff',
+    height: 35,
+    justifyContent: 'center'
+  },
+  pleaseSelect: {
+    fontSize: 16,
+    color: '#000000'
+  },
+  itemDesc: {
+    marginTop: 5,
+    width: width,
+    backgroundColor: '#ffffff'
   }
 });
 
