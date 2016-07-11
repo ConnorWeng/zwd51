8bcdb9173efe340572e75c5323554cc4bb1c69dd
Connor Weng <foamtea30@gmail.com>
2016-07-11 23:10:34 +0800

    update searchpage

--- components/SearchPage.js
+++ components/SearchPage.js
@@ -2,30 +2,50 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Text,
-  ScrollView
+  TextInput,
+  ScrollView,
+  TouchableOpacity
 } from 'react-native';
-import {
-  getTheme
-} from 'react-native-material-kit';
-
-const theme = getTheme();
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class SearchPage extends Component {
 
   render() {
     return (
-      <View style={[theme.cardStyle, styles.card]}>
-        <Text style={theme.cardTitleStyle}>Search</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.searchContainer}>
+          <View style={styles.searchInputContainer}>
+            <TextInput style={styles.searchInput} underlineColorAndroid="rgba(0,0,0,0)" placeholder="搜索..."/>
+          </View>
+          <TouchableOpacity style={styles.searchFilter} onPress={() => {}}>
+            <Icon name="ios-funnel-outline" size={30} color="#000000" />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     );
   }
 
 }
 
 const styles = StyleSheet.create({
-  card: {
-    height: 150
+  searchContainer: {
+    flexDirection: 'row'
+  },
+  searchInputContainer: {
+    flex: 5,
+    height: 35,
+    marginLeft: 20,
+    borderRadius: 20,
+    backgroundColor: '#ffffff'
+  },
+  searchInput: {
+    height: 40,
+    marginLeft: 10,
+    fontSize: 16
+  },
+  searchFilter: {
+    flex: 1,
+    alignItems: 'center'
   }
 });
 
