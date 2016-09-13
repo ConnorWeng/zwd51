b52830328c9238a3979d7070c1ce1b001ace5ed4
Connor Weng <foamtea30@gmail.com>
2016-09-13 21:37:26 +0800

    update userpage

--- components/UserPage.js
+++ components/UserPage.js
@@ -34,9 +34,9 @@ class UserPage extends Component {
           <TouchableHighlight style={{marginTop: 0}}>
             <View style={styles.headContainer}>
               <Image style={styles.headImage}
-                     source={{uri: 'http://g.hiphotos.baidu.com/zhidao/wh%3D600%2C800/sign=a5656052494a20a4314b34c1a062b41a/79f0f736afc37931ec26be69edc4b74543a91127.jpg'}}/>
+                     source={{uri: 'http://img.qq745.com/uploads/allimg/150510/1-150510223546-53.jpg'}}/>
               <View style={styles.headUserNameContainer}>
-                <Text style={styles.headUserName}>Administrator</Text>
+                <Text style={styles.headUserName}>{this.props.username}</Text>
               </View>
               <Icon style={styles.headArrow} name="ios-arrow-forward-outline" size={30} color="#000000" />
             </View>
@@ -79,9 +79,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
   },
   headImage: {
-    height: 60,
-    width: 60,
+    height: 65,
+    width: 65,
     marginLeft: 20,
+    borderRadius: 5,
   },
   headUserNameContainer: {
     flex: 8,
