591f7858b714359881a2702ba3535e134c449197
Connor Weng <foamtea30@gmail.com>
2016-10-04 15:07:31 +0800

    refactor

--- actions/service.js
+++ actions/service.js
@@ -1,14 +1,79 @@
 // TODO: handle network error
 export async function loginService(username, password) {
+  return await callPostService('/mobile_member/login', {
+    username: username,
+    password: password,
+  });
+};
+
+export async function submitOrderService(specIds, specNums, addressId,
+                                         behalfId, deliveryId, postscript,
+                                         accessToken) {
+  return await callPostService('/mobile_order/submit_order', {
+    spec_ids: specIds,
+    spec_nums: specNums,
+    address_id: addressId,
+    behalf_id: behalfId,
+    delivery_id: deliveryId,
+    postscript: postscript,
+    access_token: accessToken,
+  });
+};
+
+export async function getOrdersService(accessToken) {
+  return await callGetService('/mobile_order/index', {
+    access_token: accessToken,
+  });
+}
+
+export async function getAlipayOrderInfoService(orderId, accessToken) {
+  return await callGetService('/mobile_order/get_alipay_order_info', {
+    order_id: orderId,
+    access_token: accessToken,
+  });
+}
+
+export async function getShopsService(mkId, page) {
+  return await callGetService('/mobile_shop/index', {
+    mk_id: mkId,
+    page: page,
+  });
+}
+
+export async function getGoodsService(storeId, page) {
+  return await callGetService('/mobile_goods/index', {
+    store_id: storeId,
+    page: page,
+  });
+}
+
+import {SERVICE_URL} from "../service.json";
+
+async function callGetService(path, params) {
   let json;
   try {
-    const response = await fetch(remoteService('/mobile_member/login'), {
+    const response = await fetch(
+      remoteService(path) + buildQueryString(params));
+    json = await response.json();
+  } catch (e) {
+    json = {
+      error: true,
+      message: e.message,
+    };
+  }
+  return json;
+}
+
+async function callPostService(path, params) {
+  let json;
+  try {
+    const response = await fetch(remoteService(path), {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: `username=${username}&password=${password}`,
+      body: buildQueryString(params),
     });
     json = await response.json();
   } catch (e) {
@@ -19,91 +84,15 @@ export async function loginService(username, password) {
     };
   }
   return json;
-};
-
-export async function submitOrderService(specIds, specNums, addressId,
-                                         behalfId, deliveryId, postscript,
-                                         accessToken) {
-  let json;
-  try {
-    const response = await fetch(remoteService('/mobile_order/submit_order'), {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `spec_ids=${specIds}&spec_nums=${specNums}&address_id=${addressId}&behalf_id=${behalfId}&delivery_id=${deliveryId}&postscript=${postscript}&access_token=${accessToken}`,
-    });
-    json = await response.json();
-  } catch (e) {
-    json = {
-      error: true,
-      message: e.message,
-    };
-  }
-  return json;
-};
-
-export async function getOrdersService(accessToken) {
-  let json;
-  try {
-    const response = await fetch(remoteService('/mobile_order/index') + '&access_token=' + accessToken);
-    json = await response.json();
-  } catch (e) {
-    json = {
-      error: true,
-      message: e.message,
-    };
-  }
-  return json;
 }
 
-export async function getAlipayOrderInfoService(orderId, accessToken) {
-  let json;
-  try {
-    const response = await fetch(remoteService(
-      '/mobile_order/get_alipay_order_info') + `&order_id=${orderId}&access_token=${accessToken}`);
-    json = await response.json();
-  } catch (e) {
-    json = {
-      error: true,
-      message: e.message,
-    };
+function buildQueryString(params) {
+  let query = '';
+  for (var key in params) {
+    query += `&${key}=${params[key]}`;
   }
-  return json;
+  return query;
 }
-
-export async function getShopsService(mkId, page) {
-  let json;
-  try {
-    const response = await fetch(
-      remoteService('/mobile_shop/index') + `&mk_id=${mkId}&page=${page}`);
-    json = await response.json();
-  } catch (e) {
-    json = {
-      error: true,
-      message: e.message,
-    };
-  }
-  return json;
-}
-
-export async function getGoodsService(storeId, page) {
-  let json;
-  try {
-    const response = await fetch(
-      remoteService('/mobile_goods/index') + `&store_id=${storeId}&page=${page}`);
-    json = await response.json();
-  } catch (e) {
-    json = {
-      error: true,
-      message: e.message,
-    };
-  }
-  return json;
-}
-
-import {SERVICE_URL} from "../service.json";
 
 function remoteService(serviceName) {
   const parts = serviceName.split('/');
