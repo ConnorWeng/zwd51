211300d55d987a84e23ab6e305085ae696aae34d
Connor Weng <foamtea30@gmail.com>
2016-10-09 11:06:29 +0800

    refactor

---
+++ actions/builder.js
@@ -0,0 +1,69 @@
+import {SERVICE_URL} from "../service.json";
+
+export function serviceAction(serviceName, path, method, params) {
+  return async (dispatch) => {
+    dispatch({
+      type: serviceName + '_REQUEST',
+    });
+    let json;
+    if (method === 'GET') {
+      json = await callGetService(path, params);
+    } else if (method === 'POST') {
+      json = await callPostService(path, params);
+    }
+    dispatch({
+      type: serviceName + '_CHECK',
+      json: json,
+    });
+  };
+}
+
+async function callGetService(path, params) {
+  let json;
+  try {
+    const response = await fetch(
+      remoteService(path) + buildQueryString(params));
+    json = await response.json();
+  } catch (e) {
+    json = {
+      error: true,
+      message: e.message,
+    };
+  }
+  return json;
+}
+
+async function callPostService(path, params) {
+  let json;
+  try {
+    const response = await fetch(remoteService(path), {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: buildQueryString(params),
+    });
+    json = await response.json();
+  } catch (e) {
+    // TODO: log error and send later?
+    json = {
+      error: true,
+      message: e.message,
+    };
+  }
+  return json;
+}
+
+function buildQueryString(params) {
+  let query = '';
+  for (var key in params) {
+    query += `&${key}=${params[key]}`;
+  }
+  return query;
+}
+
+function remoteService(serviceName) {
+  const parts = serviceName.split('/');
+  return SERVICE_URL + '/index.php?app=' + parts[1] + '&act=' + parts[2];
+}
--- actions/index.js
+++ actions/index.js
@@ -1,3 +1,5 @@
+import {serviceAction} from './builder';
+
 export function changeMainTab(index) {
   return {
     type: 'CHANGE_MAIN_TAB',
@@ -102,75 +104,5 @@ export function clearSearchGoods() {
 export function clearShopGoods() {
   return {
     type: 'CLEAR_SHOP_GOODS',
-  }
-}
-
-function serviceAction(serviceName, path, method, params) {
-  return async (dispatch) => {
-    dispatch({
-      type: serviceName + '_REQUEST',
-    });
-    let json;
-    if (method === 'GET') {
-      json = await callGetService(path, params);
-    } else if (method === 'POST') {
-      json = await callPostService(path, params);
-    }
-    dispatch({
-      type: serviceName + '_CHECK',
-      json: json,
-    });
   };
-}
-
-import {SERVICE_URL} from "../service.json";
-
-async function callGetService(path, params) {
-  let json;
-  try {
-    const response = await fetch(
-      remoteService(path) + buildQueryString(params));
-    json = await response.json();
-  } catch (e) {
-    json = {
-      error: true,
-      message: e.message,
-    };
-  }
-  return json;
-}
-
-async function callPostService(path, params) {
-  let json;
-  try {
-    const response = await fetch(remoteService(path), {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: buildQueryString(params),
-    });
-    json = await response.json();
-  } catch (e) {
-    // TODO: log error and send later?
-    json = {
-      error: true,
-      message: e.message,
-    };
-  }
-  return json;
-}
-
-function buildQueryString(params) {
-  let query = '';
-  for (var key in params) {
-    query += `&${key}=${params[key]}`;
-  }
-  return query;
-}
-
-function remoteService(serviceName) {
-  const parts = serviceName.split('/');
-  return SERVICE_URL + '/index.php?app=' + parts[1] + '&act=' + parts[2];
 }
