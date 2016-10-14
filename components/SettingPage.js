67976684961afd8328842d0806183156325926d6
Connor Weng <foamtea30@gmail.com>
2016-10-15 01:16:56 +0800

    update settingpage

--- components/SettingPage.js
+++ components/SettingPage.js
@@ -3,17 +3,26 @@ import {
   StyleSheet,
   View,
   Text,
+
 } from 'react-native';
 import PrimaryButton from './PrimaryButton';
+import MyItemsGroup from './MyItemsGroup';
+import MyItem from './MyItem';
 
 class SettingPage extends Component {
 
   render() {
     return (
-      <PrimaryButton label="退出登录" onPress={() => {
-          this.props.persistor.purge();
-          this.props.navigator.pop();
-        }}/>
+      <View>
+        <MyItemsGroup>
+          <MyItem title="收货地址管理" iconName="ios-settings-outline"
+                  onPress={() => {this.props.navigator.push({AddressPage: true, addr: {}});}}/>
+        </MyItemsGroup>
+        <PrimaryButton label="退出登录" onPress={() => {
+            this.props.persistor.purge();
+            this.props.navigator.pop();
+          }}/>
+      </View>
     );
   }
 
