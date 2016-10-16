80e3a0ce83dd4e2bb5d8aa649f0bdfc2f75c8819
Connor Weng <foamtea30@gmail.com>
2016-10-17 01:02:36 +0800

    update userpage

--- components/UserPage.js
+++ components/UserPage.js
@@ -40,7 +40,6 @@ class UserPage extends Component {
                 <MyItemsGroup>
                   <MyItem title="我的订单" iconName="ios-list-box-outline"
                           onPress={() => {this.props.navigator.push({OrderPage: true});}}/>
-                    <MyItem title="淘宝订单" iconName="ios-list-box-outline"/>
                 </MyItemsGroup>
                 <MyItemsGroup>
                   <MyItem title="设置" iconName="ios-settings-outline"
