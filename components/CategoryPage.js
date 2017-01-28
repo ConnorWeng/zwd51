1f7da74bd848b6093d0ce1253a23a2d0f8791ad5
Connor Weng <foamtea30@gmail.com>
2017-01-29 00:47:25 +0800

    update categorypage

--- components/CategoryPage.js
+++ components/CategoryPage.js
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import {connect} from 'react-redux';
-import Spinner from 'react-native-loading-spinner-overlay';
+import Loading from './Loading';
 import {mapDispatchToProps} from '../actions/mapper';
 
 class CategoryPage extends Component {
@@ -63,20 +63,29 @@ class CategoryPage extends Component {
           }
         </ScrollView>
         <ScrollView style={styles.rightView}>
-          <View style={styles.rightContent}>
-            {
-              this.props.category.getNextLayerRequest.categories.map((subCate) => {
-                return (
-                  <TouchableOpacity key={subCate.cate_id} style={styles.subCategoryContainer} onPress={() => this.props.navigator.push({CategoryItemsPage: true, category: subCate})}>
-                    <Image source={{uri: 'http://www.51zwd.com/data/files/mall/template/48x48-m.png'}} style={styles.subCategoryLogo}/>
-                    <Text style={{color: '#000000'}}>{subCate.cate_mname ? subCate.cate_mname : subCate.cate_name}</Text>
-                  </TouchableOpacity>
-                );
-              })
-            }
-          </View>
+        {(() => {
+          if (this.props.category.getNextLayerRequest.isLoading) {
+            return (
+              <Loading/>
+            );
+          } else {
+            return (
+              <View style={styles.rightContent}>
+                {
+                  this.props.category.getNextLayerRequest.categories.map((subCate) => {
+                    return (
+                      <TouchableOpacity key={subCate.cate_id} style={styles.subCategoryContainer} onPress={() => this.props.navigator.push({CategoryItemsPage: true, category: subCate})}>
+                        <Image source={{uri: 'http://www.51zwd.com/data/files/mall/template/48x48-m.png'}} style={styles.subCategoryLogo}/>
+                        <Text style={{color: '#000000'}}>{subCate.cate_mname ? subCate.cate_mname : subCate.cate_name}</Text>
+                      </TouchableOpacity>
+                    );
+                  })
+                }
+              </View>
+            );
+          }
+        })()}
         </ScrollView>
-        <Spinner visible={this.props.category.getNextLayerRequest.isLoading}/>
       </View>
     );
   }
